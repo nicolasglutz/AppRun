46d08eab9308ad533089f5f6175fb275aca10cdc
oronk <varotom4@gmail.com>
2022-09-10 13:31:45 +0200

    Magnet sensor working

--- Bootcamp/app/src/main/java/ch/bfh/bootcamp/activities/MainActivity.java
+++ Bootcamp/app/src/main/java/ch/bfh/bootcamp/activities/MainActivity.java
@@ -7,13 +7,19 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
 
 import ch.bfh.bootcamp.R;
@@ -24,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textView_qr_content;
 
-
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         textView_qr_content = findViewById(R.id.qr_content);
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
         //Set callback if qrcode is detected
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
@@ -54,6 +62,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mCodeScanner.startPreview();
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mSensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                float[] mag = sensorEvent.values;
+                double betrag = Math.sqrt(mag[0] * mag[0] + mag[1] * mag[1] + mag[2] *
+                        mag[2]);
+                betrag = betrag / 100;
+                betrag = betrag > 100 ? 100 : betrag;
+                ProgressBar progressBar = findViewById(R.id.progressBar);
+                progressBar.setMax(100);
+                progressBar.setProgress( (int) Math.round(betrag));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+            }
+        }, mSensor, mSensorManager.SENSOR_DELAY_NORMAL);
+
 
     }
     @Override
