fda7e68886ca8118ea048091a9cf203259a94c5f
glutzNicolas <nicolas.glutz@bbzsogr.ch>
2022-09-10 11:33:18 +0200

    init bootcamp project with default view

---
+++ Bootcamp/app/src/main/java/ch/bfh/bootcamp/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ch.bfh.bootcamp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.bfh.bootcamp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
