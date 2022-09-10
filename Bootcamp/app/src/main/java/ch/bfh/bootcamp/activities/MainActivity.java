c20c8c25e1868c7d59463756b7f8bae87eb003b9
glutzNicolas <nicolas.glutz@bbzsogr.ch>
2022-09-10 12:08:58 +0200

    Update MainActivity.java

--- Bootcamp/app/src/main/java/ch/bfh/bootcamp/activities/MainActivity.java
+++ Bootcamp/app/src/main/java/ch/bfh/bootcamp/activities/MainActivity.java
@@ -1,16 +1,48 @@
 package ch.bfh.bootcamp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 import ch.bfh.bootcamp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CodeScanner mCodeScanner;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        mCodeScanner.startPreview();
+        /*scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });*/
+
     }
 }
