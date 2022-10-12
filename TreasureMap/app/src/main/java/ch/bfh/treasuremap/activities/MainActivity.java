c973b72bec960e1a238ff82c1bcd00070a3142eb
nicolasglutz <54477001+nicolasglutz@users.noreply.github.com>
2022-10-12 11:14:35 +0200

    app works and has main functionality

--- TreasureMap/app/src/main/java/ch/bfh/treasuremap/activities/MainActivity.java
+++ TreasureMap/app/src/main/java/ch/bfh/treasuremap/activities/MainActivity.java
@@ -1,23 +1,40 @@
 package ch.bfh.treasuremap.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.MotionEvent;
 
+import org.osmdroid.LocationListenerProxy;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 import org.osmdroid.wms.WMSTileSource;
 
 import java.util.ArrayList;
@@ -30,42 +47,140 @@ public class MainActivity extends AppCompatActivity {
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map = null;
 
+    private MyLocationNewOverlay myLocationOverlay;
+    private IMapController mapController;
+
+    private static Drawable myIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+        //Request permissions
+        requestPermissionsIfNecessary(new String[]{
+                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.INTERNET
+        });
 
 
         Context ctx = this.getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
+
+        myIcon = AppCompatResources.getDrawable(getApplicationContext(), org.osmdroid.wms.R.drawable.person);
+
         map = findViewById(R.id.map);
+
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.getController().setZoom(18.0);
 
-        requestPermissionsIfNecessary(new String[]{
-                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.INTERNET
-        });
         map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
         map.setMultiTouchControls(true);
 
 
-        CompassOverlay compassOverlay = new CompassOverlay(this, map);
-        compassOverlay.enableCompass();
-        map.getOverlays().add(compassOverlay);
+        //init the compass
+//        CompassOverlay compassOverlay = new CompassOverlay(this, map);
+//        compassOverlay.enableCompass();
+//        map.getOverlays().add(compassOverlay);
 
-        GeoPoint point = new GeoPoint(45.845557, 26.170010);
+        //init my current location
+        this.myLocationOverlay = new MyLocationNewOverlay(new GpsMyLocationProvider(getApplicationContext()), map);
+        myLocationOverlay.enableFollowLocation();
+        myLocationOverlay.enableMyLocation();
 
-        Marker startMarker = new Marker(map);
-        startMarker.setPosition(point);
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
-        map.getOverlays().add(startMarker);
+        map.getOverlays().add(this.myLocationOverlay);
 
-        map.getController().setCenter(point);
+
+        //init the touch listener
+        Overlay touchOverlay = new Overlay(this) {
+            @Override
+            public void draw(Canvas arg0, MapView arg1, boolean arg2) {
+
+            }
+
+            @Override
+            public boolean onSingleTapConfirmed(final MotionEvent e, final MapView mapView) {
+
+                Projection proj = mapView.getProjection();
+                GeoPoint loc = (GeoPoint) proj.fromPixels((int) e.getX(), (int) e.getY());
+
+                GeoPoint point = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+
+                Marker startMarker = new Marker(mapView);
+
+                startMarker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+                    @Override
+                    public boolean onMarkerClick(Marker marker, MapView mapView) {
+
+                        mapView.getOverlays().remove(marker);
+
+                        otherMarkerClicked = true;
+
+                        mapView.invalidate();
+
+                        return false;
+                    }
+                });
+
+                if (!otherMarkerClicked) {
+                    startMarker.setPosition(point);
+                    startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+                    mapView.getOverlays().add(startMarker);
+                    mapView.invalidate();
+
+                }
+
+                otherMarkerClicked = false;
+
+                return true;
+            }
+        };
+        map.getOverlays().add(touchOverlay);
+
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                GeoPoint point = new GeoPoint(location.getLatitude(), location.getLongitude());
+                if(!firstLocationSet){
+                    map.getController().setCenter(point);
+                    firstLocationSet = true;
+                }
+
+
+
+                if (lastLocationMarker != null)
+                    map.getOverlays().remove(lastLocationMarker);
+
+                lastLocationMarker = new Marker(map);
+                lastLocationMarker.setPosition(point);
+                lastLocationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+
+
+
+                lastLocationMarker.setIcon(myIcon);
+                map.getOverlays().add(lastLocationMarker);
+                map.invalidate();
+
+            }
+        });
+
     }
 
+
+    private static Marker lastLocationMarker;
+
+    private static boolean firstLocationSet = false;
+    private static boolean otherMarkerClicked = false;
 
 
     @Override
@@ -75,6 +190,7 @@ public class MainActivity extends AppCompatActivity {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        myLocationOverlay.enableMyLocation();
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
@@ -85,12 +201,14 @@ public class MainActivity extends AppCompatActivity {
         //if you make changes to the configuration, use
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
+        myLocationOverlay.disableMyLocation();
+        myLocationOverlay.disableFollowLocation();
         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         ArrayList<String> permissionsToRequest = new ArrayList<>();
         for (int i = 0; i < grantResults.length; i++) {
